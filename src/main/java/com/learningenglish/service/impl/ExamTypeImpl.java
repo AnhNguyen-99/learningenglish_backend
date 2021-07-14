5e2ffbe7b55b0b7c26143e46af0744e5bf8706fd
minhtran-99 <tranminh16111999@gmail.com>
2021-07-14 22:04:17 +0700

    commit ExamTypeImpl

---
+++ src/main/java/com/learningenglish/service/impl/ExamTypeImpl.java
@@ -0,0 +1,45 @@
+package com.learningenglish.service.impl;
+
+import com.learningenglish.entiy.ExamType;
+import com.learningenglish.repository.ExamTypeRepository;
+import com.learningenglish.service.ExamTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ExamTypeImpl implements ExamTypeService {
+
+    @Autowired
+    private ExamTypeRepository examRepon;
+    @Override
+    public List<ExamType> findAll() {
+        return examRepon.findAll();
+    }
+
+    @Override
+    public ExamType findById(int id) {
+        return examRepon.findById(id).get();
+    }
+
+    @Override
+    public ExamType save(ExamType examType) {
+        if(examType.getId() != null){
+            ExamType saveExam = examRepon.findById(examType.getId()).get();
+        }
+        return examRepon.save(examType);
+    }
+
+    @Override
+    public Boolean delete(int id) {
+        try {
+            examRepon.deleteById(id);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
