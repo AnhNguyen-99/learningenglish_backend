6046f84d99da91a47934ddbb21453374da849ddb
Anh-99 <nguyenleanh3101@gmail.com>
2021-07-19 00:18:46 +0700

    config swagger2

--- src/main/java/com/learningenglish/config/SwaggerConfig.java
+++ src/main/java/com/learningenglish/config/SwaggerConfig.java
@@ -1,64 +1,50 @@
-//package com.learningenglish.config;
-//
-//import org.springframework.beans.factory.annotation.Configurable;
-//import org.springframework.context.annotation.Bean;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.*;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spi.service.contexts.SecurityContext;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//@Configurable
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    public static final String AUTHORIZATION_HEADER = "Authorization";
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(getApiInfo())
-//                .securityContexts(Arrays.asList(securityContext()))
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//
-//                .build();
-//    }
-//
-//    private ApiInfo getApiInfo() {
-//        Contact contact = new Contact("learnenglish.com", "https://learningenglish.com", "nguyenleanh3101@gmail.com");
-//        return new ApiInfoBuilder()
-//                .title("Spring Boot Swagger")
-//                .description("Spring Boot Swagger LearningEnglish")
-//                .version("1.0.0")
-//                .license("Apache 2.0")
-//                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
-//                .contact(contact)
-//                .build();
-//    }
-//
-//    private ApiKey apiKey() {
-//        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
-//    }
-//
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder()
-//                .securityReferences(defaultAuth())
-//                .build();
-//    }
-//
-//    List<SecurityReference> defaultAuth() {
-//        AuthorizationScope authorizationScope
-//                = new AuthorizationScope("global", "accessEverything");
-//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//        authorizationScopes[0] = authorizationScope;
-//        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-//    }
-//}
+package com.learningenglish.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+@Configurable
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket() {
+        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+        aParameterBuilder.name("Authorization")                 // name of header
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")               // type - header
+                .defaultValue("Basic em9uZTpteXBhc3N3b3Jk")        // based64 of - zone:mypassword
+                .required(true)                // for compulsory
+                .build();
+        java.util.List<Parameter> aParameters = new ArrayList<>();
+        aParameters.add(aParameterBuilder.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo())
+                .globalOperationParameters(aParameters);
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo("TITLE",
+                "DESCIPRION",
+                "VERSION",
+                "TERMS OF SERVICE URL",
+                new Contact("NAME", "URL", "EMAIL"),
+                "LICENSE", "LICENSE URL", Collections.emptyList());
+    }
+}
--- src/main/java/com/learningenglish/security/WebSecurityConfig.java
+++ src/main/java/com/learningenglish/security/WebSecurityConfig.java
@@ -6,20 +6,19 @@ import com.learningenglish.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
@@ -34,6 +33,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public JwtAuthTokenFilter authenticationJwtTokenFilter() {
         return new JwtAuthTokenFilter();
     }
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+            "/configuration/security", "/swagger-ui.html", "/webjars/**", "/login", "/account/create", "/verify", "/contact", "/verify/resetPass",
+            "/restPass", "/ws-chat", "/v1/banner/getBannersActive", "/v1/account/resetPassword", "/v1/account/settingAccount"        // other public endpoints of your API may be appended to this array
+    };
 
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
@@ -57,21 +63,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**", "/api/user/**", "/api/**", "/api/exam_type/**","/api/exam/**","/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+                .antMatchers("/api/auth/**", "/api/user/**", "/api/**", "/api/exam_type/**", "/api/exam/**", "/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+                        "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
+//                .antMatchers("/api/auth/**", "/api/user/**", "/api/**", "/api/exam_type/**","/api/exam/**","/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .logout().and()
                 .exceptionHandling()
                 .authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs",
-                "/swagger-ui.html",
-                "/webjars/**");
     }
 
 }
